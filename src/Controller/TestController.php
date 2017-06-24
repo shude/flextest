e7bf66232d8c0f208cc4c17a8bafc9129dc051df
Shude <babah@kekx.ru>
2017-06-24 22:12:33 +0300

    Initial commit

---
+++ src/Controller/TestController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017 Vladimir Tarkhanov.
+ * @author    Vladimir Tarkhanov <shu-de@kekx.ru>
+ * Date: 24.06.2017
+ * Time: 21:39
+ */
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class TestController
+{
+    /**
+     * @return Response
+     * @Route("/test", name="test")
+     */
+    public function test()
+    {
+        return new Response('It\'s work!');
+    }
+}
